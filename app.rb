e99a2627a2d42c06852f5480d4eefc6315058d61
Zachery Moneypenny <whazzmaster@gmail.com>
2013-08-24 13:15:56 -0500

    Refactor button labels into hash

--- app.rb
+++ app.rb
@@ -12,9 +12,22 @@ Shoes.app do
 		'f' => Interface::MusicalInputButton.new(@app, 280, 70, 50, 50, rgb(65, 105, 225)),
 	}
 
+	labels = {
+		'a' => { left: 85, top: 125},
+		's' => { left: 155, top: 125},
+		'd' => { left: 225, top: 125},
+		'f' => { left: 300, top: 125}
+	}
+
 	Interface::ButtonManager.new(@app, key_mappings).listen
 
 	stack do
 		key_mappings.each {|key,button| button.element }
+
+		labels.each do |char, coordinates|
+			label = title char
+			label.move coordinates[:left], coordinates[:top]
+		end
+
 	end
  end
