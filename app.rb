f90b7428bb94eb2d99a164424b3e6ea1a94709b9
Zachery Moneypenny <whazzmaster@gmail.com>
2013-08-24 11:12:17 -0500

    Micromidi experiments

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
+require 'midi'
+
 Shoes.app do
 	stack do
 		@title = para "Woo!"
-		@button = button "paste from clipboard"
-		@button.click do
-			@title = para clipboard()
-		end
+		@button = button("play!") {
+				@o = UniMIDI::Output.use(:first)
+
+				MIDI.using(@o) do
+
+				  note "C"
+				  off 
+
+				  cc 5, 120
+
+				  play "C3", 0.5
+
+				end 
+		}
+
+		fill black
+		oval 10, 40, 50, 50
 	end
  end
