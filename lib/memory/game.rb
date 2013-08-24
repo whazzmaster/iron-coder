94b09db806af48ed0046b93d9b8f7a6b326cc0b2
Jeff Casimir <jeff@casimircreative.com>
2013-08-24 11:29:48 -0500

    Stubbed out game process

--- lib/memory/game.rb
+++ lib/memory/game.rb
@@ -1,5 +1,23 @@
 module Memory
   class Game
+    def initialize(game_length)
 
+    end
+
+    def start
+      self
+    end
+
+    def sequence
+      Array.new(10)
+    end
+
+    def play(move)
+      true
+    end
+
+    def won?
+      true
+    end
   end
 end
--- test/memory/unit/game_test.rb
+++ test/memory/unit/game_test.rb
@@ -5,5 +5,13 @@ class GameTest < Minitest::Test
     assert Memory::Game
   end
 
-
+  def test_it_can_be_won
+    game = Memory::Game.new(10).start
+    expected_sequence = game.sequence
+    assert_equal 10, expected_sequence.size
+    expected_sequence.each do |next_note|
+      assert game.play(next_note)
+    end
+    assert game.won?
+  end
 end
